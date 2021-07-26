d138e9dff39aa467fc3ff6026b858806d81d6491
Melkorich <Kirill.Lavrov92@gmail.com>
2021-07-27 01:50:10 +0300

    header added and main slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function(){
+
+
+  $('.header-slider').slick({
+    dots: true,
+    prevArrow:'<button type="button" class="slick-prev"><img src="images/icons/slider-arrow-left.svg" alt="prev"></button>',
+    nextArrow:'<button type="button" class="slick-next"><img src="images/icons/slider-arrow-right.svg" alt="next"></button>',
+
+  });
+
  
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 const { src, dest, watch, parallel, series } = require('gulp');
-const scss = require('gulp-sass');
+const scss = require('gulp-dart-sass');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const del = require('del')
 const browserSync = require('browser-sync').create();
+
 
 function browsersync(){
   browserSync.init({
@@ -31,6 +32,7 @@ function styles(){
 function scripts(){
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
